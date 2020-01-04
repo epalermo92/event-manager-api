a45bcafe983a8e34cc6769c96ac2b5ab06a41341
Emanuele <emanuelepalermo1992@gmail.com>
2020-01-04 18:26:47 +0100

    Event model and event builder done

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -1,7 +1,94 @@
 <?php
 
+namespace AppBundle\Entity;
 
 class Event
 {
+    /** @var string $place */
+    private $place;
 
+    /** @var \DateTime $date */
+    private $date;
+
+    /** @var string $name */
+    private $name;
+
+    /** @var int $numMaxParticipants */
+    private $numMaxParticipants;
+
+    /** @var string $description */
+    private $description;
+
+    /** @var \LegalIdentity|\NaturalIdentity $organizer */
+    private $organizer;
+
+    /** @var \NaturalIdentity[] $participants */
+    private $participants;
+
+    public function __construct($place, $date, $name, $numMaxParticipants, $description, $organizer, $participants)
+    {
+        $this->place = $place;
+        $this->date = $date;
+        $this->name = $name;
+        $this->numMaxParticipants = $numMaxParticipants;
+        $this->description = $description;
+        $this->organizer = $organizer;
+        $this->participants = $participants;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlace()
+    {
+        return $this->place;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumMaxParticipants()
+    {
+        return $this->numMaxParticipants;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return \LegalIdentity|\NaturalIdentity
+     */
+    public function getOrganizer()
+    {
+        return $this->organizer;
+    }
+
+    /**
+     * @return \NaturalIdentity[]
+     */
+    public function getParticipants()
+    {
+        return $this->participants;
+    }
 }
