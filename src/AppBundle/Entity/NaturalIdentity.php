58edc3d1d6948da964f5fd5a69ac43c012794634
Gianfranco <gianfranco.pagliaroto@facile.it>
2020-01-07 17:56:55 +0100

    Doctrine ORM in Legal and Natural Identity

--- src/AppBundle/Entity/NaturalIdentity.php
+++ src/AppBundle/Entity/NaturalIdentity.php
@@ -2,6 +2,11 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
 class NaturalIdentity extends AbstractIdentity
 {
     public function __construct($name, $surname)
