a958bd3eda9dea95bcc9d81c7b6140dd7948bf2d
emanuele <emanuele.palermo@facile.it>
2020-01-08 17:20:08 +0100

    getDependencies in LoadEvent fixtures

--- src/AppBundle/DataFixtures/LoadEvent.php
+++ src/AppBundle/DataFixtures/LoadEvent.php
@@ -3,12 +3,20 @@
 namespace AppBundle\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class LoadEvent extends Fixture
+class LoadEvent extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
         // TODO: Implement load() method.
+    }
+
+    public function getDependencies(): array
+    {
+        return array(
+           LoadIdentity::class,
+        );
     }
 }
