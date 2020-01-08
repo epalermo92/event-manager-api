54620fc6800c019a79667e949273578337db7ee1
Gianfranco <gianfranco.pagliaroto@facile.it>
2020-01-08 12:15:55 +0100

    AbstractIdentity correction

--- src/AppBundle/Entity/AbstractIdentity.php
+++ src/AppBundle/Entity/AbstractIdentity.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity()
  * @ORM\Table(name="Identity")
  * @ORM\InheritanceType("SINGLE_TABLE")
- * @ORM\DiscriminatorColumn("discriminator", type="string", length=15)
  */
 abstract class AbstractIdentity
 {
