0ecff862e520ec9480c3571bd30c805e0f645ecd
Gianfranco <gianfranco.pagliaroto@facile.it>
2020-01-08 11:48:55 +0100

    db_start.sh first attempt

---
+++ db_start.sh
@@ -0,0 +1,4 @@
+docker exec -ti event-manager-app_php_1  php bin/console doctrine:database:drop --force
+docker exec -ti event-manager-app_php_1  php bin/console doctrine:database:create
+docker exec -ti event-manager-app_php_1  php bin/console doctrine:schema:validate
+docker exec -ti event-manager-app_php_1  php bin/console doctrine:schema:update --force --complete --dump-sql
