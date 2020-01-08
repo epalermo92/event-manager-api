a4ab1059937b01c8e602be1d89079fe8ff2fc6bb
emanuele <emanuele.palermo@facile.it>
2020-01-08 15:59:28 +0100

    DataFixtures skeleton corrected

---
+++ src/DataFixtures/LoadEvent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadEvent extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // TODO: Implement load() method.
+    }
+}
---
+++ src/DataFixtures/LoadIdentity.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadIdentity extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // TODO: Implement load() method.
+    }
+}
