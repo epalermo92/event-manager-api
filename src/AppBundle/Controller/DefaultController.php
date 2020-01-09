38ba57f2762d924e8107d3fc99853fdf6384e1da
Gianfranco <gianfranco.pagliaroto@facile.it>
2020-01-09 13:06:44 +0100

    Create Identity form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
@@ -31,6 +32,29 @@ class DefaultController extends Controller
             ->getForm();
 
         return $this->render('default/create_event.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/identity_creation", name="identity-creation")
+     */
+    public function createIdentity()
+    {
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class)
+            ->add('surname', TextType::class)
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Natural' => 'natural',
+                    'Legal' => 'legal',
+                ],
+                'placeholder' => 'Add type',
+                'required' => true,
+            ])
+            ->getForm();
+
+        return $this->render('default/create_identity.html.twig', [
             'form' => $form->createView(),
         ]);
     }
