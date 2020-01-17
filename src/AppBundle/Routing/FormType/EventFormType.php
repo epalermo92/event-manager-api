a691afd432bbd66412f155b8455f0218c7a91fc7
emanuele <emanuele.palermo@facile.it>
2020-01-17 12:40:35 +0100

    EventFormType validation

--- src/AppBundle/Routing/FormType/EventFormType.php
+++ src/AppBundle/Routing/FormType/EventFormType.php
@@ -3,46 +3,90 @@
 namespace AppBundle\Routing\FormType;
 
 use AppBundle\Entity\AbstractIdentity;
+use AppBundle\Entity\NaturalIdentity;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use AppBundle\Entity\NaturalIdentity;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class EventFormType extends AbstractRequestType
 {
+    
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name', TextType::class, [
-            'label' => 'Nome',
-            'required' => true
-        ]);
+        $builder->add(
+            'name',
+            TextType::class,
+            [
+                'label' => 'Nome',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ]
+        );
 
-        $builder->add('description', TextType::class, [
-            'label' => 'descrizione',
-            'required' => true
-        ]);
+        $builder->add(
+            'description',
+            TextType::class,
+            [
+                'label' => 'descrizione',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ]
+        );
 
-        $builder->add('place', TextType::class, [
-            'label' => 'Luogo',
-            'required' => true
-        ]);
+        $builder->add(
+            'place',
+            TextType::class,
+            [
+                'label' => 'Luogo',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ]
+        );
 
-        $builder->add('num_max_participants', NumberType::class, [
-            'label' => 'Num max participanti',
-            'required' => true
-        ]);
+        $builder->add(
+            'num_max_participants',
+            NumberType::class,
+            [
+                'label' => 'Num max participanti',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ]
+        );
 
-        $builder->add('organizer',EntityType::class, [
-            'class' => AbstractIdentity::class,
-            'label' => 'organizer',
-            'required' => true
-        ]);
+        $builder->add(
+            'organizer',
+            EntityType::class,
+            [
+                'class' => AbstractIdentity::class,
+                'label' => 'organizer',
+                'required' => true,
+                'constraints' => [
+                    new  NotBlank(),
+                ],
+            ]
+        );
 
-        $builder->add('participants', EntityType::class, [
-            'class' => NaturalIdentity::class,
-            'label' => 'Num max participanti',
-            'required' => true
-        ]);
+        $builder->add(
+            'participants',
+            EntityType::class,
+            [
+                'class' => NaturalIdentity::class,
+                'label' => 'Num max participanti',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ]
+        );
     }
 }
