fb6fea671ada25440308dac8cc5fc30748021836
emanuele <emanuele.palermo@facile.it>
2020-01-20 18:08:21 +0100

    Script run tests

---
+++ run_tests.sh
@@ -0,0 +1,6 @@
+docker exec -ti event-manager-app_php_1  php bin/console doctrine:database:drop --force
+docker exec -ti event-manager-app_php_1  php bin/console doctrine:database:create
+docker exec -ti event-manager-app_php_1  php bin/console doctrine:schema:validate
+docker exec -ti event-manager-app_php_1  php bin/console doctrine:schema:update --force --complete --dump-sql
+docker exec -ti event-manager-app_php_1  php bin/console doctrine:fixtures:load --no-interaction
+docker exec event-manager-app_php_1 vendor/bin/phpunit --colors=always --do-not-cache-result tests/
