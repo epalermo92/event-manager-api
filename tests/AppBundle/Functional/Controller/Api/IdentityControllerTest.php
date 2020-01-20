ab36ffbbb089820e1f4a3488da250e20d62f489e
Gianfranco <gianfranco.pagliaroto@facile.it>
2020-01-20 16:33:37 +0100

    postIdentitiesAction done

--- tests/AppBundle/Functional/Controller/Api/IdentityControllerTest.php
+++ tests/AppBundle/Functional/Controller/Api/IdentityControllerTest.php
@@ -4,7 +4,53 @@
 namespace Tests\AppBundle\Functional\Controller\Api;
 
 
-class IdentityControllerTest
-{
+use AppBundle\Entity\AbstractIdentity;
+use AppBundle\Routing\FormType\IdentityFormType;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+class IdentityControllerTest extends WebTestCase
+{
+    public function testPostIdentityAction(): void
+    {
+        $client = self::createClient();
+
+        $entityManager = $client
+            ->getContainer()
+            ->get('doctrine.orm.default_entity_manager');
+
+        $oldIdentitiesNumber = count(
+            $entityManager
+                ->getRepository(AbstractIdentity::class)
+                ->findAll()
+        );
+
+        $form = $client
+            ->getContainer()
+            ->get('form.factory')
+            ->create(IdentityFormType::class);
+
+        $client
+            ->request(
+                'POST',
+                '/api/identities',
+                [
+
+                    'name' => 'Pippo',
+                    'surname' => 'Pluto',
+                    'codiceFiscale' => 'PPOPLT23R19D245G',
+                    'type' => 'natural',
+                ]
+            );
+        $form->handleRequest($client->getRequest());
+        $this->assertTrue($form->isSubmitted());
+        $this->assertTrue($form->isValid());
+
+        $newIdentitiesNumber = count(
+            $entityManager
+                ->getRepository(AbstractIdentity::class)
+                ->findAll()
+        );
+
+        $this->assertSame($oldIdentitiesNumber + 1, $newIdentitiesNumber);
+        $this->assertStringContainsString('', $client->getResponse()->getContent());
 }
